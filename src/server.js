bc7763fe079fd3a177254f3b7451056fc2cc1017
gaurab7 <96043792+gaurab7@users.noreply.github.com>
2025-06-13 16:57:20 +0000

    modified server.js

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import weatherRoutes from './routes/weatherRoute.js'
+import cityVerify from './middleware/cityVerify.js'
 
 const app = express()
 
@@ -9,7 +10,7 @@ app.use(express.json())
 const PORT = process.env.PORT || 8848
 
 //ROUTES
-app.use('/weather', weatherRoutes)
+app.use('/weather',cityVerify, weatherRoutes)
 
 
 //Start the server
