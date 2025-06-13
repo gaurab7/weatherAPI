bfb1890caa2d1a8ccba5f8cf3d5086523386487c
gaurab7 <96043792+gaurab7@users.noreply.github.com>
2025-06-13 10:12:50 +0000

    setup weather api endpoint

--- src/routes/weatherRoute.js
+++ src/routes/weatherRoute.js
@@ -1,5 +1,10 @@
 import express from 'express'
+import { fetchWeather } from '../services/weatherServices.js'
 
 const router = express.Router()
+
+router.get('/', (req, res)=>{
+       res.json(fetchWeather())
+})
 
 export default router
