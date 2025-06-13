2cf85334665828bf0aac01de60bd3f0c38632d62
gaurab7 <96043792+gaurab7@users.noreply.github.com>
2025-06-13 16:54:21 +0000

    completed route setup

--- src/routes/weatherRoute.js
+++ src/routes/weatherRoute.js
@@ -1,10 +1,16 @@
 import express from 'express'
-import { fetchWeather } from '../services/weatherServices.js'
+import { weatherData } from '../controllers/weatherController.js'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const router = express.Router()
 
-router.get('/', (req, res)=>{
-       res.json(fetchWeather())
+router.get('/', async (req, res)=>{
+       const city  = req.query.q
+       const apiKey = process.env.apiKey
+       const formatted_data = await weatherData(city, apiKey)
+       res.json({ current_weather: formatted_data})//no need to use ${} as formatted_data is an object, if u do so, it will return [object Object]
 })
 
 export default router
