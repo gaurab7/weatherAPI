c7063eb84cd1d5cb31da1acd407a8c6130337b43
gaurab7 <96043792+gaurab7@users.noreply.github.com>
2025-06-13 15:58:52 +0000

    middleware for verification

---
+++ src/middleware/cityVerify.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import { verifyCity } from '../services/cityServices.js'
+
+
+const router = express.Router()
+
+
+//need to format this
+router.get('/', async (req, res, next)=>{
+    const city  = req.query.q
+    const verification = await verifyCity(city)
+    if(!verification) {
+        console.log('invalid city')
+        return res.status(400).json({ msg: 'Invalid city' });
+    }
+    else
+    {
+        next()
+    }
+})
+
+export default router
