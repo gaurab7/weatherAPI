d32e509c012cf9e7272b7c8731f54e0afee2ebd6
gaurab7 <96043792+gaurab7@users.noreply.github.com>
2025-06-14 04:00:39 +0000

    updated limit

--- src/middleware/rate_limiter.js
+++ src/middleware/rate_limiter.js
@@ -2,7 +2,7 @@ import rateLimit from 'express-rate-limit'
 
 export const limiter = rateLimit({
   windowMs: 10 * 60 * 1000, // 10 minutes
-  max: 1, //10 requests per  minute
+  max: 10, //10 requests per 10 minute
   message: 'You have reached the request limits. Please try again later',
   standardHeaders: true,//new format apparently
   legacyHeaders: false,//older one 
