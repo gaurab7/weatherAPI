2a68a179405154d5b7dbcb4d6787bcd7f243a688
gaurab7 <96043792+gaurab7@users.noreply.github.com>
2025-09-02 13:54:35 +0000

    tommorrow and day after done

--- public/forecast.js
+++ public/forecast.js
@@ -8,13 +8,25 @@ localStorage.setItem('forecastdataCache', JSON.stringify(forecastdata))//for sof
 
 document.addEventListener('DOMContentLoaded', ()=>{
     const forecastCard = document.getElementById('detailed-forecast')
-    const todayCard = document.getElementById('detailed-today')
-    const astro = document.getElementById('today-astro')
-    const hourlyChart = document.getElementById('today-hourly')
-    //clearing previous info if any
-    todayCard.innerHTML = ''
+    const detailedCard = document.getElementById('detailed-card')
+    const astro = document.getElementById('astro-card')
+    const hourlyChart = document.getElementById('hourly')
+    const tDetailedCard = document.getElementById('t-detailed-card')
+    const tAstro = document.getElementById('t-astro-card')
+    const tHourlyChart = document.getElementById('t-hourly')
+    const DDetailedCard = document.getElementById('d-detailed-card')
+    const DAstro = document.getElementById('d-astro-card')
+    const DHourlyChart = document.getElementById('d-hourly')
+    //clearing previous info if any--causes errors if not done
+    detailedCard.innerHTML = ''
+    tDetailedCard.innerHTML= ''
+    DDetailedCard.innerHTML= ''
     astro.innerHTML = ''
+    tAstro.innerHTML = ''
+    DAstro.innerHTML = ''
     hourlyChart.innerHTML = ''
+    tHourlyChart.innerHTML = ''
+    DHourlyChart.innerHTML = ''
     console.log(forecastdata)
     let days = []
     let todayData = []
@@ -36,8 +48,9 @@ document.addEventListener('DOMContentLoaded', ()=>{
     
     //#region today
     //today-->current weather only
-    todayCard.innerHTML = `
-     <li><strong><label><i class="bi bi-umbrella-fill" style="font-size:24px; color: darkgrey;" title="Condition"></i></label></strong> 
+    document.getElementById('dateToday').innerText = `${days[0].date}`
+    detailedCard.innerHTML = `
+     <li><strong><label><i class="bi bi-umbrella-fill" style="font-size:24px; color: #4A90E2;" title="Condition"></i></label></strong> 
      <img src="${forecastdata.iconLink}" style="width:30px; height:30px; vertical-align: middle; padding-bottom:0;margin:0;">
      <p style="padding:0; font-size:0.7rem;">${forecastdata.condition}</p>
      </li>
@@ -47,10 +60,10 @@ document.addEventListener('DOMContentLoaded', ()=>{
        <li><strong><label><i class="bi bi-clouds-fill" title="Cloud Coverage" style="font-size:24px; color:#AAB8C0;"></i></label></strong> ${forecastdata.cloud}%</li>
     `
     //todat--> hourly forecast + other forecast info for today
-    todayCard.innerHTML += `
-         <li><strong><label><i class="bi bi-thermometer-high" title="Max. Temp"  style="font-size:24px; color:#FF7043;"></i></label></strong> ${days[0].day.maxtemp_c}°C</li>
+    detailedCard.innerHTML += `
+         <li><strong><label><i class="bi bi-thermometer-high" title="Max. Temp"  style="font-size:24px; color:#FF6B6B;"></i></label></strong> ${days[0].day.maxtemp_c}°C</li>
         <li><strong><label><i class="bi bi-thermometer-half" title="Avg. Temp" style="font-size:24px; color:#616161;"></i></label></strong> ${days[0].day.avgtemp_c}°C</li>
-         <li><strong><label><i class="bi bi-thermometer-low" title="Min. Temp" style="font-size:24px; color:#29B6F6;"></i></label></strong> ${days[0].day.mintemp_c}°C</li>
+         <li><strong><label><i class="bi bi-thermometer-low" title="Min. Temp" style="font-size:24px; color:#4ABDAC;"></i></label></strong> ${days[0].day.mintemp_c}°C</li>
          <li><strong><label><i class="bi bi-cloud-rain-fill" title="Chance of Rain" style="font-size:24px; color:#29B6F6; "></i></label></strong> ${days[0].day.daily_chance_of_rain}%</li>
          <li><strong><label><i class="bi bi-snow" title="Chance of Snow" style="font-size:24px; color:#81D4FA; "></i></label></strong> ${days[0].day.daily_chance_of_snow}%</li>
          <li><strong><label><img src="/assets/uv.png" title="UV Index" alt="UV Index" style="width:30px; height:30px;"></label></strong> ${days[0].day.uv}</li>
@@ -70,22 +83,96 @@ document.addEventListener('DOMContentLoaded', ()=>{
       <li><strong><label>Moon Up: </label></strong> ${days[0].astro.is_moon_up ? 'Yes' : 'No'}</li>
     `
 
+
+    //creating a chart
+    makeChart(hourlyChart.id ,days, 0)
+//#endregion today
+
+
+    //#region changeView
+    const day = document.getElementById('day')
+    const slider = document.getElementById('day-slider')
+    day.innerText = "Moist Weather Today"
+    document.getElementById('today-section').classList.add('show')//on load after all content is fomatted, load today
+    const sections = [
+         document.getElementById('forecast-section'),
+         document.getElementById('today-section'),
+         document.getElementById('tommorrow-section'),
+         document.getElementById('day-after-section')
+    ]
+    slider.addEventListener('input', ()=>{
+        const sectionInd = parseInt(slider.value)
+        sections.forEach((element, ind)=>{
+            //element and index of sections array
+            if(ind == sectionInd){
+                //classlist to modify css classes of the el
+                switch(ind){
+                    case 0:
+                        day.innerText = "3-day Moist Forecast"
+                        break
+                    case 1:
+                        day.innerText = "Moist Weather Today"
+                        document.getElementById('dateToday').style.display = 'flex'
+                        break
+                    case 2:
+                        day.innerText = "Moist Weather Tommorrow"
+                        document.getElementById('dateTom').style.display = 'flex'
+                        break
+                    case 3:
+                        day.innerText = "Moist Weather Day After Tommorrow"
+                        document.getElementById('dateDay').style.display = 'flex'
+                        break
+                    default:
+                        day.innerText = "No Content Senor"
+                        break
+                }
+                element.classList.add('show')
+            }
+            else{
+                switch(ind){
+                    case 1:
+                        document.getElementById('dateToday').style.display = 'none'
+                        break
+                    case 2:
+                        document.getElementById('dateTom').style.display = 'none'
+                        break
+                    case 3:
+                        document.getElementById('dateDay').style.display = 'none'
+                        break
+                }
+                element.classList.remove('show')
+            }
+        })
+    })
+    //#endregion changeView
+
+    //#region f-tommor-dayafter
+    document.getElementById('dateTom').innerText = `${days[1].date}`
+    document.getElementById('dateDay').innerText = `${days[2].date}`
+    addContent(tDetailedCard,tAstro,tHourlyChart,days,1)//tommorrow
+    addContent(DDetailedCard,DAstro,DHourlyChart,days,2)//day after tommorrow
+
+    //#endregion f-tommor-dayafter
+})
+
+const charts ={}
+
+function makeChart(canvasid, days, ind)
+{
+    const ctx = document.getElementById(canvasid).getContext('2d')
      let labels 
      let feelsData
      let rainData 
      let tempData
+    if (charts[canvasid]) {charts[canvasid].destroy()}
     //today-->hourly
-    days[0].hour.forEach(hourData => {
-      labels = days[0].hour.map(h => h.time.slice(11, 16))//HH:MM format
-      tempData = days[0].hour.map(h => h.temp_c)
-      feelsData = days[0].hour.map(h => h.feelslike_c)
-      rainData = days[0].hour.map(h => h.chance_of_rain)
+    days[ind].hour.forEach(hourData => {
+      labels = days[ind].hour.map(h => h.time.slice(11, 16))//HH:MM format
+      tempData = days[ind].hour.map(h => h.temp_c)
+      feelsData = days[ind].hour.map(h => h.feelslike_c)
+      rainData = days[ind].hour.map(h => h.chance_of_rain)
     })
-
-    //creating a chart
-    const ctx = hourlyChart.getContext('2d')//context is a thing chartjs uses to draw on the canvas
-    
-    new Chart(ctx, {
+     charts[canvasid] =  new Chart(ctx, {
         type: 'line',//type of chart--bar,line.etc
         data: { //data for the chart
             labels: labels,
@@ -152,7 +239,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
                 tooltip: {
                     callbacks: {
                         label: function(context) {
-                            const h = days[0].hour[context.dataIndex]
+                            const h = days[ind].hour[context.dataIndex]
                             return `${context.dataset.label}: ${context.dataset.label.includes('Rain') ? h.chance_of_rain + '%' : h.temp_c + '°C'} | Feels Like: ${h.feelslike_c}°C | Humidity: ${h.humidity}%`
                         }
                     }
@@ -160,50 +247,43 @@ document.addEventListener('DOMContentLoaded', ()=>{
             }
         }
     })
- 
-//#endregion today
+}
 
-    //#region forecast
-    const day = document.getElementById('day')
-    const slider = document.getElementById('day-slider')
-    day.innerText = "Moist Weather Today"
-    document.getElementById('today-section').classList.add('show')//on load after all content is fomatted, load today
-    const sections = [
-         document.getElementById('forecast-section'),
-         document.getElementById('today-section'),
-         document.getElementById('tommorrow-section'),
-         document.getElementById('day-after-section')
-    ]
-    
-    slider.addEventListener('input', ()=>{
-        const sectionInd = parseInt(slider.value)
-        sections.forEach((element, ind)=>{
-            //element and index of sections array
-            if(ind == sectionInd){
-                //classlist to modify css classes of the el
-                switch(ind){
-                    case 0:
-                        day.innerText = "3-day Moist Forecast"
-                        break
-                    case 1:
-                        day.innerText = "Moist Weather Today"
-                        break
-                    case 2:
-                        day.innerText = "Moist Weather Tommorrow"
-                        break
-                    case 3:
-                        day.innerText = "Moist Weather Day After Tommorrow"
-                        break
-                    default:
-                        day.innerText = "No Content Senor"
-                        break
-                }
-                element.classList.add('show')
-            }
-            else{
-                element.classList.remove('show')
-            }
-        })
-    })
-    //#endregion forecast
-})
+function addContent(detailedCard, astro, hourlyChart,days, dayCount)
+{
+    //previously used forecastdata which was an obj this days is an array with 3 el
+    //from browser console saw what days had then extracted 
+    detailedCard.innerHTML = `
+     <li><strong><label><i class="bi bi-umbrella-fill" style="font-size:24px; color: #4A90E2;" title="Condition"></i></label></strong> 
+     <img src="${days[dayCount].day.condition.icon}" style="width:30px; height:30px; vertical-align: middle; padding-bottom:0;margin:0;">
+     <p style="padding:0; font-size:0.7rem;">${days[dayCount].day.condition.text}</p>
+     </li>
+     <li><strong><label><i class="bi bi-droplet-half" title="Humidity" style="font-size:24px; color:#29B6F6;"></i></label></strong> ${days[dayCount].day.avghumidity}%</li>
+     <li><strong><label><i class="bi bi-thermometer-high" title="Max. Temp"  style="font-size:24px; color:#FF6B6B;"></i></label></strong> ${days[dayCount].day.maxtemp_c}°C</li>
+     <li><strong><label><i class="bi bi-thermometer-half" title="Avg. Temp" style="font-size:24px; color:#616161;"></i></label></strong> ${days[dayCount].day.avgtemp_c}°C</li>
+     <li><strong><label><i class="bi bi-thermometer-low" title="Min. Temp" style="font-size:24px; color:#4ABDAC;"></i></label></strong> ${days[dayCount].day.mintemp_c}°C</li>
+     <li><strong><label><i class="bi bi-eye-fill" title="Avg. Visibility" style="font-size:24px; color: darkgrey;"></i></label></strong> ${days[dayCount].day.avgvis_km} km</li>
+     <li><strong><label><i class="bi bi-cloud-rain-fill" title="Chance of Rain" style="font-size:24px; color:#29B6F6; "></i></label></strong> ${days[dayCount].day.daily_chance_of_rain}%</li>
+     <li><strong><label><i class="bi bi-snow" title="Chance of Snow" style="font-size:24px; color:#81D4FA; "></i></label></strong> ${days[dayCount].day.daily_chance_of_snow}%</li>
+     <li><strong><label><img src="/assets/uv.png" title="UV Index" alt="UV Index" style="width:30px; height:30px;"></label></strong> ${days[dayCount].day.uv}</li>
+     <li><strong><label><i class="bi bi-cloud-drizzle-fill" title="Total Precipitation" style="font-size:24px; color:#29B6F6;"></i></label></strong> ${days[dayCount].day.totalprecip_mm} mm</li>
+    <li><strong><label><i class="bi bi-cloud-snow-fill" title="Total Snow" style="font-size:24px; color:darkgrey; "></i></label></strong> ${days[dayCount].day.totalsnow_cm} cm</li>
+     <li><strong><label>Rain?</label></strong> ${days[dayCount].day.daily_will_it_rain ? 'Yes' : 'No'} </li>
+     <li><strong><label>Snow?</label></strong> ${days[dayCount].day.daily_will_it_snow ? 'Yes' : 'No'} </li>
+     <li><strong><label>Max Wind: </label></strong> ${days[dayCount].day.maxwind_kph} kph</li>
+    `
+    // Astro info
+    astro.innerHTML = `
+      <li><strong><label><img src="/assets/sunrise.png" alt="Sunrise" style="width:30px; height:30px;border:none;"></label></strong> ${days[dayCount].astro.sunrise}</li>
+      <li><strong><label><img src="/assets/sea.png" alt="Sunset" style="width:30px; height:30px;"></label></strong> ${days[dayCount].astro.sunset}</li>
+      <li><strong><label><img src="/assets/moon.png" alt="Sunrise" style="width:30px; height:30px;"></i></label></strong> ${days[dayCount].astro.moonrise}</li>
+      <li><strong><label><img src="/assets/sky.png" alt="Sunrise" style="width:30px; height:30px;"></label></strong> ${days[dayCount].astro.moonset}</li>
+      <li><strong><label>Moon Phase: </label></strong> ${days[dayCount].astro.moon_phase}</li>
+      <li><strong><label>Moon Illumination: </label></strong> ${days[dayCount].astro.moon_illumination}%</li>
+      <li><strong><label>Sun Up: </label></strong> ${days[dayCount].astro.is_sun_up ? 'Yes' : 'No'}</li>
+      <li><strong><label>Moon Up: </label></strong> ${days[dayCount].astro.is_moon_up ? 'Yes' : 'No'}</li>
+    `
+
+
+    makeChart(hourlyChart.id,days, dayCount)
+}
