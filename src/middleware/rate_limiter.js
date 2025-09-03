d10cd4bc869ec4643c24b1592c7321b93e9bdc35
gaurab7 <96043792+gaurab7@users.noreply.github.com>
2025-09-03 01:03:15 +0000

    phone size

--- src/middleware/rate_limiter.js
+++ src/middleware/rate_limiter.js
@@ -2,7 +2,7 @@ import rateLimit from 'express-rate-limit'
 
 export const limiter = rateLimit({
   windowMs: 10 * 60 * 1000, // 10 minutes
-  max: 10, //10 requests per 10 minute
+  max: 80, //80 requests per 10 minute-->8 per minute 
   message: 'You have reached the request limits. Please try again later',
   standardHeaders: true,//new format apparently
   legacyHeaders: false,//older one 
